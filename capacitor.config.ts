654b2589072f7364e76935993c73c3d3d7fd30e7
Kendrick Chan <kenchan0824@gmail.com>
2023-06-10 10:54:06 +0100

    change package name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.ancoatsoft.strikes',
   appName: 'Strikes Alert',
   webDir: 'dist',
   server: {
